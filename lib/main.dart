1882a7478e42ee975e10a4ea799e5f6706d7871a
uttamXD <raiuttam88.ur@gmail.com>
2023-06-22 00:03:57 +0545

    edit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'mySplashScreen.dart';
+
 void main() {
-  runApp(
-      MaterialApp(debugShowCheckedModeBanner: false, home: MySplashScreen()));
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MySplashScreen(),
+  ));
 }
---
+++ lib/mySplashScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MySplashScreen extends StatefulWidget {
+  const MySplashScreen({super.key});
+
+  @override
+  State<MySplashScreen> createState() => _MySplashScreenState();
+}
+
+class _MySplashScreenState extends State<MySplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return MySplashScreen();
+  }
+}
